dea59039427efffdeb90d528198cb3295b631b83
vogef3 <florian.vogel@students.bfh.ch>
2019-12-09 21:21:43 +0100

    Updates

---
+++ precache-manifest.62ae93ffafd8dc52d2b7ab5af1881f06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7fd971137a302e00f9f9f954aca45a",
+    "url": "/psychology-values/index.html"
+  },
+  {
+    "revision": "f1814f30f8aeb41c2145",
+    "url": "/psychology-values/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d9401b4beab354da4052",
+    "url": "/psychology-values/static/js/2.a38d59da.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/psychology-values/static/js/2.a38d59da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1814f30f8aeb41c2145",
+    "url": "/psychology-values/static/js/main.2cfee1f4.chunk.js"
+  },
+  {
+    "revision": "8a9cf31577faf1e15d1c",
+    "url": "/psychology-values/static/js/runtime-main.1ea46b72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/psychology-values/static/media/logo.5d5d9eef.svg"
+  }
+]);
